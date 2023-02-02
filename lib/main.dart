433dd936785593d26af16a86fcf3caa56c7567c1
Noor Illahi <ujjannoorillahi@gmail.com>
2023-02-02 21:17:29 +0500

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
-  runApp(
-    const MaterialApp(),
-  );
+  runApp(const OurFlutterApp());
+  print("Build from Main Function");
+}
+
+class OurFlutterApp extends StatelessWidget {
+  const OurFlutterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print('Build from internal Widget');
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text('Our Flutter App'),
+          backgroundColor: Colors.teal,
+        ),
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.lightGreen,
+            child: const Center(
+              child: Text('Sukkur IBA'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
